a6d90cff68fb0e2ed6432928654646c2d2a99258
ianfagundes <ianfag@icloud.com>
2023-06-28 11:48:29 -0300

    debug possible error.

--- fastlane/extensions/Signing.rb
+++ fastlane/extensions/Signing.rb
@@ -29,13 +29,42 @@ platform :ios do
     match(git_branch: branch, app_identifier: bundle_ids, type: "appstore", api_key_path: api_key_path)
 end
   
+# def recreate_api_key_file
+#     api_key_json = ENV["APPSTORECONNECT_API_KEY_JSON"]
+#     api_key_hash = JSON.parse(api_key_json)
+#     file_path = File.expand_path("AppStoreConnectAPIKey.json") 
+#     File.open(file_path, "w") do |f|
+#         f.write(api_key_hash.to_json)
+#     end
+#     return file_path
+# end
 def recreate_api_key_file
-    api_key_json = ENV["APPSTORECONNECT_API_KEY_JSON"]
-    api_key_hash = JSON.parse(api_key_json)
-    file_path = File.expand_path("AppStoreConnectAPIKey.json") 
-    File.open(file_path, "w") do |f|
-        f.write(api_key_hash.to_json)
-    end
-    return file_path
+  # Add the file path of your JSON file
+  file_path = 'AppStoreConnectAPIKey.json'
+  
+  # Print out a statement indicating that file is about to be read
+  puts "Reading file: #{file_path}"
+  
+  # Read the file's contents
+  file_contents = File.read(file_path)
+  
+  # Print out the contents of the file
+  puts "File contents: #{file_contents}"
+  
+  # Continue with your existing code...
+  
+  # existing code here
+  # json_contents = JSON.parse(file_contents)
+  # ...
+
+  # Rescuing the JSON::ParserError to provide more debug information
+  begin
+    json_contents = JSON.parse(file_contents)
+  rescue JSON::ParserError => e
+    puts "Failed to parse JSON due to #{e.message}"
+    # Reraise the exception
+    raise
+  end
 end
+
 end
