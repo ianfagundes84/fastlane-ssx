1e973ef2e71697740f1045a3319f099d9ee98790
ianfagundes <ianfag@icloud.com>
2023-06-28 16:39:25 -0300

    Testing3.

--- fastlane/extensions/Signing.rb
+++ fastlane/extensions/Signing.rb
@@ -10,9 +10,11 @@ platform :ios do
   desc "Fetch the Development, Distribution Certificates"
   lane :fetch_certificates do |options|
     target_name = options[:target_name]
-    fetch_certificates(target_name, [Identifiers::WHITELABELSSX_DEV]) # Whitelabel     
-    fetch_certificates(target_name, [Identifiers::WHITELABELSSX_DEV_COPY]) # Whitelabel copy
+    api_key_path = options[:api_key_path]
+    fetch_certificates(target_name, [Identifiers::WHITELABELSSX_DEV], api_key_path) # Whitelabel     
+    fetch_certificates(target_name, [Identifiers::WHITELABELSSX_DEV_COPY], api_key_path) # Whitelabel copy
   end
+  
 
   desc "Update profiles on apple developer portal when adding new devices"
   lane :update_profiles do |options|
@@ -23,12 +25,11 @@ platform :ios do
     end
   end
 
-  def fetch_certificates(branch, bundle_ids)
-    api_key_path = recreate_api_key_file
+  def fetch_certificates(branch, bundle_ids, api_key_path)
     match(git_branch: branch, app_identifier: bundle_ids, type: "development", api_key_path: api_key_path)
     match(git_branch: branch, app_identifier: bundle_ids, type: "appstore", api_key_path: api_key_path)
-end
-  
+  end
+
 # def recreate_api_key_file
 #     api_key_json = ENV["APPSTORECONNECT_API_KEY_JSON"]
 #     api_key_hash = JSON.parse(api_key_json)
@@ -38,22 +39,5 @@ end
 #     end
 #     return file_path
 # end
-def recreate_api_key_file
-  puts "Current directory: #{Dir.pwd}"
-  api_key = {
-    "key_id": "8Z96WD6G8A",
-    "issuer_id": "69a6de84-fb7c-47e3-e053-5b8c7c11a4d1",
-    "key": "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgjDwq/3vqO0uqsXqQ/y3BDiCGb8fG91XmlUIYy9s1wDagCgYIKoZIzj0DAQehRANCAAThsyhUsknewhmoqzlgri9nFzCz7lLUke929wxGHJ7fJw7sW5pq/ZfzMPSWH5+ngtHlf+MitVsX4/RobCg5th8U\n-----END PRIVATE KEY-----",
-    "in_house": false
-  }
-  
-  file_path = "AppStoreConnectAPIKey.json"
-  File.open(file_path, "w") do |f|
-    f.write(api_key.to_json)
-  end
-
-  return file_path # Ensure that the file path is returned, not the parsed JSON
-end
-
 
 end
