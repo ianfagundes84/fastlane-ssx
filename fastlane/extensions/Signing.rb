2b8dcefdd8bffd2d5126b8733338c55f41d0b15a
ianfagundes <ianfag@icloud.com>
2023-06-28 16:56:09 -0300

    Testing4.

--- fastlane/extensions/Signing.rb
+++ fastlane/extensions/Signing.rb
@@ -19,25 +19,13 @@ platform :ios do
   desc "Update profiles on apple developer portal when adding new devices"
   lane :update_profiles do |options|
     target_name = options[:target_name]
-    # if prompt(text: "Ao realizar esse procedimento, os profiles atuais serão revogados e substituídos, deseja continuar?", boolean: true)
-      update_profiles(target_name, [Identifiers::WHITELABELSSX_DEV])
-      update_profiles(target_name, [Identifiers::WHITELABELSSX_DEV_COPY]) 
-    # end
+    update_profiles(target_name, [Identifiers::WHITELABELSSX_DEV])
+    update_profiles(target_name, [Identifiers::WHITELABELSSX_DEV_COPY]) 
   end
 
   def fetch_certificates(branch, bundle_ids, api_key_path)
     match(git_branch: branch, app_identifier: bundle_ids, type: "development", api_key_path: api_key_path)
     match(git_branch: branch, app_identifier: bundle_ids, type: "appstore", api_key_path: api_key_path)
   end
-
-# def recreate_api_key_file
-#     api_key_json = ENV["APPSTORECONNECT_API_KEY_JSON"]
-#     api_key_hash = JSON.parse(api_key_json)
-#     file_path = File.expand_path("AppStoreConnectAPIKey.json") 
-#     File.open(file_path, "w") do |f|
-#         f.write(api_key_hash.to_json)
-#     end
-#     return file_path
-# end
 
 end
